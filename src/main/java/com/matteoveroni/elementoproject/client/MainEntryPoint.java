0e29d2288f1d75bf1129743186b564e40460f25e
Matteo Veroni <matver87@gmail.com>
2018-05-15 09:34:35 +0200

    first commit

---
+++ src/main/java/com/matteoveroni/elementoproject/client/MainEntryPoint.java
@@ -0,0 +1,11 @@
+package com.matteoveroni.elementoproject.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class MainEntryPoint implements EntryPoint{
+
+	@Override
+	public void onModuleLoad() {
+		// How to call and use WebPage?
+	}
+}
---
+++ src/main/java/com/matteoveroni/elementoproject/client/WebPage.java
@@ -0,0 +1,14 @@
+package com.matteoveroni.elementoproject.client;
+
+import elemental2.dom.HTMLElement;
+import org.jboss.gwt.elemento.core.IsElement;
+//import static org.jboss.gwt.elemento.processor.context.DataElementInfo.Kind.HTMLElement;
+import org.jboss.gwt.elemento.template.Templated;
+
+@Templated("PaginaUno.html")
+public abstract class WebPage implements IsElement<HTMLElement> {
+
+	static WebPage create() {
+		return new Templated_WebPage();
+	}
+}
