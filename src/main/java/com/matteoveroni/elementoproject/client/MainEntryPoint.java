6ef22a32d9410b54ea194c9ea93d2dce77b285d0
Matteo Veroni <matver87@gmail.com>
2018-05-16 15:36:23 +0200

    minor changes

--- src/main/java/com/matteoveroni/elementoproject/client/MainEntryPoint.java
+++ src/main/java/com/matteoveroni/elementoproject/client/MainEntryPoint.java
@@ -1,13 +1,13 @@
 package com.matteoveroni.elementoproject.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import elemental2.dom.DomGlobal;
+import static elemental2.dom.DomGlobal.document;
 
 public class MainEntryPoint implements EntryPoint{
 
 	@Override
 	public void onModuleLoad() {
 		WebPage webPage = WebPage.create();
-		DomGlobal.document.body.appendChild(webPage.asElement());
+		document.body.appendChild(webPage.asElement());
 	}
 }
