85a86eb85ca04c74db06e3d4c38e599baeedc47c
Matteo Veroni <matver87@gmail.com>
2018-06-01 19:26:03 +0200

    removed useless code

--- src/main/java/com/matteoveroni/elementoproject/client/WebPage.java
+++ src/main/java/com/matteoveroni/elementoproject/client/WebPage.java
@@ -2,7 +2,6 @@ package com.matteoveroni.elementoproject.client;
 
 import elemental2.dom.HTMLElement;
 import org.jboss.gwt.elemento.core.IsElement;
-//import static org.jboss.gwt.elemento.processor.context.DataElementInfo.Kind.HTMLElement;
 import org.jboss.gwt.elemento.template.Templated;
 
 @Templated("WebPage.html")
